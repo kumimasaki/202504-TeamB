0df95c40a624b38f10843e9a1e3961752f10551a
kumimasaki <masaki.kmkm@gmail>
2025-05-23 08:42:43 +0900

    プロジェクト作成

---
+++ TeamBProject/src/main/java/ec/com/TeamBProjectApplication.java
@@ -0,0 +1,13 @@
+package ec.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamBProjectApplication.class, args);
+	}
+
+}
