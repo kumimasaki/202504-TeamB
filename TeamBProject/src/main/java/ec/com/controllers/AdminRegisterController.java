94f96fb8c1f42c282b8a52b7ae4c219098aea696
LRfei1122 <liruofei0@gmail.com>
2025-06-10 11:49:38 +0900

    AdminRegisterの完成

--- TeamBProject/src/main/java/ec/com/controllers/AdminRegisterController.java
+++ TeamBProject/src/main/java/ec/com/controllers/AdminRegisterController.java
@@ -31,37 +31,31 @@ public class AdminRegisterController {
 	public String adminRegisterConfirm(@RequestParam String adminName, @RequestParam String adminEmail,
 			@RequestParam String password, @RequestParam String adminPassword, Model model) {
 
-		// モデルに値を入れて渡す
-		Admin admin = (Admin) session.getAttribute("registerAdmin");
 		// パスワード一致チェック
 		if (!password.equals(adminPassword)) {
 			model.addAttribute("error", "パスワードが一致しません。");
 			// 元の画面に戻る
 			return "admin_register.html";
+		} else {
+			model.addAttribute("adminName", adminName);
+			model.addAttribute("adminEmail", adminEmail);
+			model.addAttribute("adminPassword", adminPassword);
+			return "admin_confirm_register.html";
 		}
-		return "admin_confirm_register.html";
 
 	}
 
 	// 登録処理 @PostMapping("/admin/register/process")
 	@PostMapping("/admin/register/process")
 	public String adminRegisterProcess(@RequestParam String adminName, @RequestParam String adminEmail,
-			@RequestParam String password) {
+			@RequestParam String adminPassword, Model model) {
 		// adiminを作成したら、ログイン画面へ移動
-		if (adminService.createAdmin(adminName, adminEmail, password)) {
+		if (adminService.createAdmin(adminName, adminEmail, adminPassword)) {
 			return "redirect:/admin/login";
 		} else {
 			// そうでない、登録画面に戻る
 			return "redirect:/admin/register";
 		}
 	}
-	/*
-	 * service層 AdminServise // 新規会員登録作成 public boolean createAdmin(String
-	 * adminName, String adminEmail, String adminPassword) { //
-	 * もし、findByAdminEmail(adminEmail) == nullだったら登録処理 if
-	 * (AdminDao.findByAdminEmail(adminEmail) == null) { // saveメソッドを使用し、登録処理
-	 * AdminDao.save(new Account(adminName, adminEmail, adminPassword)); //
-	 * boolean式のtrue 保存できる return true; } else { // 保存できない false return false; } }
-	 */
 
 }
