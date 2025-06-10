a535ea9bc4675e01cf345d6dda06d6da4d16e5fd
TK812000 <KASE.TAKUYA@mct-japan.co.jp>
2025-06-10 17:07:54 +0900

    パスワード変更機能の不具合修正

--- TeamBProject/src/main/java/ec/com/controllers/UserChangePasswordController.java
+++ TeamBProject/src/main/java/ec/com/controllers/UserChangePasswordController.java
@@ -12,38 +12,44 @@ import ec.com.services.UserService;
 
 @Controller
 public class UserChangePasswordController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	// 画面の表示
-	@GetMapping ("/user/password/change")
+	@GetMapping("/user/change/password")
 	public String getChangePasswordPage() {
-		return"user_password_change.html";
+		return "user_password_change.html";
 	}
-	
-	//パスワード変更処理
-	@PostMapping("/user/change/password/complete")
+
+	// パスワード変更処理
+	@PostMapping("/user/change/password/process")
 	public String ChangePasswordComplete(@RequestParam String userEmail, @RequestParam String userPassword,
 			@RequestParam String passwordConfirm, Model model) {
-		//渡されたEmailを検索
-		User user =userService.findByUserEmail(userEmail);
-		//Emailが存在すれば処理を続行
-		if(user != null) {
-			if(userPassword.equals(passwordConfirm)) {
-				//成功時、パスワードの変更処理を行いログイン画面へ
+		// 渡されたEmailを検索
+		User user = userService.findByUserEmail(userEmail);
+		// Emailが存在すれば処理を続行
+		if (user != null) {
+			if (userPassword.equals(passwordConfirm)) {
+				// 成功時、パスワードの変更処理を行いログイン画面へ
 				user.setUserPassword(userPassword);
 				userService.updateUser(user);
-				return "user_login.html";
-			}else {
-				//失敗時
-				model.addAttribute("pass-err","パスワードが一致しません");
+				return "user_change_password_complete.html";
+			} else {
+				// 失敗時
+				model.addAttribute("pass-err", "パスワードが一致しません");
+				model.addAttribute("userEmail", userEmail);
 				return "user_password_change.html";
 			}
-		}else{
+		} else {
 			// 存在しない場合メール入力画面へ
 			return "user_password_reset";
 		}
 	}
-}
+	// パスワード変更完了画面の表示
+	@GetMapping("/user/change/password/complete")
+	public String getChangePasswordCompletePage() {
+		return "user_change_password_complete.html";
+	}
 
+}
