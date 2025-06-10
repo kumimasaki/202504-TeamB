e8ca81506d866b0df251237caaa9c714c009c35f
ryuulea <learyuu1999@gmail.com>
2025-06-10 11:24:29 +0900

    講座情報登録画面と機能

---
+++ TeamBProject/src/main/java/ec/com/controllers/AdminLessonRegisterController.java
@@ -0,0 +1,79 @@
+package ec.com.controllers;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import ec.com.model.dao.LessonDao;
+import ec.com.model.entity.Lesson;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class AdminLessonRegisterController {
+
+	@Autowired
+	private LessonDao lessonDao;
+
+	@GetMapping("/admin/lesson/register")
+	public String showRegisterForm() {
+		return "admin_register_lesson.html";
+	}
+
+	@PostMapping("/admin/lesson/register/create")
+	public String registerLesson(
+			@RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+			@RequestParam("startTime") @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTime,
+			@RequestParam("finishTime") @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime finishTime,
+			@RequestParam("lessonName") String lessonName,
+			@RequestParam("lessonDetail") String lessonDetail,
+			@RequestParam("lessonFee") Integer lessonFee,
+			@RequestParam("imageName") MultipartFile imageFile,
+			HttpSession session) throws IOException {
+
+		// ファイル名を取得
+		String imageName = imageFile.getOriginalFilename();
+
+		// classpath の static/img フォルダを取得
+		File imgDir = new ClassPathResource("static/img").getFile();
+
+		// 保存先のファイルを作成
+		File saveFile = new File(imgDir, imageName);
+
+		// ファイルを保存
+		imageFile.transferTo(saveFile);
+
+		// adminId をセッションから取得
+		Long adminId = (Long) session.getAttribute("adminId");
+
+		// Lesson エンティティにセット
+		Lesson lesson = new Lesson();
+		lesson.setStartDate(startDate);
+		lesson.setStartTime(startTime);
+		lesson.setFinishTime(finishTime);
+		lesson.setLessonName(lessonName);
+		lesson.setLessonDetail(lessonDetail);
+		lesson.setLessonFee(lessonFee);
+		lesson.setImageName(imageName);
+		lesson.setRegisterDate(new Timestamp(System.currentTimeMillis()));
+		lesson.setAdminId(adminId);
+
+		// DB 保存
+		lessonDao.save(lesson);
+		
+		// 一覧画面へリダイレクト
+		return "redirect:/admin/lesson/all";
+	}
+}
+
+
