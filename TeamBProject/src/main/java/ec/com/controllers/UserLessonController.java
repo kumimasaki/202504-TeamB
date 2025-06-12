91da6e4c5ccabc78123c6de06f54512de3346068
xietianMctJapan <708825737@qq.com>
2025-06-12 17:52:54 +0900

    Revert "update"
    This reverts commit 752166d95182d4d8a907d7d680e075352584de8e.

--- TeamBProject/src/main/java/ec/com/controllers/UserLessonController.java
+++ TeamBProject/src/main/java/ec/com/controllers/UserLessonController.java
@@ -2,9 +2,7 @@ package ec.com.controllers;
 
 import java.io.Console;
 import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -209,11 +207,6 @@ public class UserLessonController {
 		return "redirect:/lesson/show/cart";
 	}
 	
-	/**
-	 * 支払い方法選択画面を表示するメソッド（GET）
-	 * ログインユーザーかどうかを確認し、未ログインならログイン画面にリダイレクト。
-	 * セッション内の講座リストが存在しない場合はメニュー画面に戻る。
-	 */
 	@GetMapping("/lesson/request")
 	public String applySelectPayment(HttpSession session,
 									 Model model) {
@@ -275,11 +268,7 @@ public class UserLessonController {
 		return "user_confirm_apply_detail.html";
 	}
 	
-	/**
-	 * 支払い方法を選択し、確認画面へ遷移するメソッド（POST）
-	 * 選択された支払い方法に応じて説明を表示。
-	 * また、講座の合計金額を計算し、セッションに保存。
-	 */
+	
 	@PostMapping("/lesson/pay")
 	public String applyComplete(@RequestParam("stripeEmail") String stripeEmail,
 								HttpSession session,
@@ -303,34 +292,5 @@ public class UserLessonController {
 		
 		session.removeAttribute("list");
 		return "user_apply_complete.html";
-	}
-	
-	/**
-	 * 支払い処理完了後の処理メソッド（POST）
-	 * Stripe支払い情報を受け取り、取引履歴とアイテムをDBに保存。
-	 * カート情報をセッションから削除し、完了画面を表示。
-	 */
-	@GetMapping("/lesson/menu/search")
-	public String getMenuSearch(@RequestParam("keyword") String keyword,
-								Model model) {
-		// ログイン状態をチェック
-		User loginUser = (User) session.getAttribute("loginUserInfo");
-
-		if (loginUser != null) {
-			// ログイン済みの場合
-			model.addAttribute("loginFlg", true);
-			model.addAttribute("userName", loginUser.getUserName());
-		} else {
-			// 未ログインの場合
-			model.addAttribute("loginFlg", false);
-		}
-
-		// 現在時刻以降の講座のみ取得
-		LocalDate localDate = LocalDate.now();
-		LocalTime localTime = LocalTime.now();
-		List<Lesson> lessonList = lessonDao.findByLessonNameContainingAndDateTimeCondition(keyword, localDate, localTime);
-		model.addAttribute("lessonList", lessonList);
-
-		return "user_menu.html";
 	}
 }
